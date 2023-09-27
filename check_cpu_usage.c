b7cd2ca493653eaec7b3d507530d38f40436e770
root <tkoizumi3@gatech.edu>
2023-09-27 04:08:10 +0000

    open stat file and print line

--- check_cpu_usage.c
+++ check_cpu_usage.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 256
 
 int main()
 {
-    printf("Starting check_cpu_usage.c\n");
+    FILE *file = fopen("/proc/stat", "r");
+    if (file == NULL)
+    {
+        perror("Error opening /proc/stat");
+        return 1;
+    }
+
+    char line[BUFFER_SIZE];
+    fgets(line, sizeof(line), file);
+    printf("Line: %s\n", line);
+
+    fclose(file);
+
+    return 0;
 }
