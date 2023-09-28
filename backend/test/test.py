5af7f48a005344a6e18fb9f9ca5534c353dbe6ff
tkoizumi <takayuki.koizumi@gmail.com>
2023-09-27 20:25:33 -0400

    print result

--- backend/test/test.py
+++ backend/test/test.py
@@ -1,13 +1,15 @@
 import subprocess
 
 
-def test(function_name:str):
+def test(function_name: str):
     print("Testing...")
     try:
-        result = subprocess.run(["../monitor/build/check_cpu_usage", function_name], stdout=subprocess.PIPE, text=True)
+        result = subprocess.run(
+            ["../monitor/build/check_cpu_usage", function_name], stdout=subprocess.PIPE, text=True)
         output = result.stdout
         return output
     except subprocess.CalledProcessError as e:
         return {"error": str(e)}
 
-print("CPU Usage: ",test("get_usage"))
+
+print("CPU Usage: ", test("get_usage"))
